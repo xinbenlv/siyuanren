3984ff7a53d464513e1c05e2d7175e9f0cc487ae
Zainan Victor Zhou <zzn@zzn.im>
2013-03-25 23:24:26 -0700

    init

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+
+/*
+ * GET home page.
+ */
+TITLE = 'Siyuan Ren'
+
+exports.index = function(req, res){
+  res.render('index', { title: TITLE });
+};
+exports.login = function(req, res){
+  res.render('login', { title: TITLE });
+};
+exports.logout = function(req, res){
+  res.render('logout', { title: TITLE });
+};
+exports.profile = function(req, res){
+  res.render('profile', { title: TITLE });
+};
+
