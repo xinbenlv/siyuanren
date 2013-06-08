7dde58c784bcbe4e115ee3713e356ca7d9071c27
Zainan Victor Zhou <zzn@zzn.im>
2013-06-08 15:57:39 -0700

    Add MongoDB, read and modify the peopletable

--- api/index.js
+++ api/index.js
@@ -1,30 +1,16 @@
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+
 var log = console.log;
-
-/**
- *
- * @deprecated Use query of people table.
- *
- * Method handling a normal query.
- *
- * @param {object} req Request for a query.
- * @param {object} res Response for a query.
- */
-exports.query = function(req, res) {
-  req.accepts(['html', 'json']);
-  console.log(req);
-
-  var sampleTableData = {
-    'headers': ['name', 'year', 'dept', 'job', 'residency city'],
-    'rows': [
-      ['刘备', '158', '卖草鞋系', '蜀汉 皇帝', 'Zhuzhou, Hebei, China'],
-      ['关羽', '160', '卖大枣系', '蜀汉 寿亭侯', 'Yuncheng, Shanxi, China'],
-      ['张飞', '163', '卖肉系', '蜀汉 车骑将军', 'Zhuzhou, Hebei, China']
-    ]
-  };
-
-  res.send(sampleTableData);
-};
-
+var USERNAME = 'testuser';
+var PASSWORD = 'testpass';
+var MONGO_HOST = 'alex.mongohq.com';
+var MONGO_PORT = '10077';
+var MONGO_DBNAME = 'app14616351';
+var MONGO_LOCAL_URL = 'mongodb://' +
+  USERNAME + ':' + PASSWORD +
+  '@' + MONGO_HOST + ':' + MONGO_PORT + '/' + MONGO_DBNAME;
+var mongoUri = process.env.MONGOHQ_URL || MONGO_LOCAL_URL;
 
 var loadData = function() {
   var metadata = [
@@ -59,6 +45,32 @@ var loadData = function() {
 
 /**
  *
+ * @deprecated Use query of people table.
+ *
+ * Method handling a normal query.
+ *
+ * @param {object} req Request for a query.
+ * @param {object} res Response for a query.
+ */
+exports.query = function(req, res) {
+  req.accepts(['html', 'json']);
+  //console.log(req);
+
+  var sampleTableData = {
+    'headers': ['name', 'year', 'dept', 'job', 'residency city'],
+    'rows': [
+      ['刘备', '158', '卖草鞋系', '蜀汉 皇帝', 'Zhuzhou, Hebei, China'],
+      ['关羽', '160', '卖大枣系', '蜀汉 寿亭侯', 'Yuncheng, Shanxi, China'],
+      ['张飞', '163', '卖肉系', '蜀汉 车骑将军', 'Zhuzhou, Hebei, China']
+    ]
+  };
+
+  res.send(sampleTableData);
+};
+
+
+/**
+ *
  * Method handling a normal peoplequery.
  *
  * @param {object} req Request for a query.
@@ -67,12 +79,37 @@ var loadData = function() {
 exports.peopletable = function(req, res) {
   req.accepts(['html', 'json']);
   if ('query' in req && 'tablename' in req.query) {
-    log(req.query);
-    res.send('ok');
+    // TODO(zzn): use QMongDB as promises.
+    var mongo = require('mongodb');
+    mongo.Db.connect(mongoUri, function(err, db) {
+      var k = 'data.' + (parseInt(req.query.id) - 1).toString() +
+        '.values.' + req.query.colname;
+      var v = req.query.newvalue;
+      var tmp = {};
+      tmp[k] = v;
+      logger.debug('tmp = ' + JSON.stringify(tmp));
+      var collection = db.collection('peopletable');
+      collection.update({ },
+        { $set: tmp }, {safe: true}, function(err, ret) {
+        logger.debug(err);
+        logger.debug(ret);
+        res.send('ok');
+      });
+    });
+
   } else {
-    res.send(
-      loadData()
-    );
+    // TODO(zzn): use QMongDB as promises.
+    var mongo = require('mongodb');
+    console.log('DBG: mongoUri=' + mongoUri);
+    mongo.Db.connect(mongoUri, function(err, db) {
+      var collection = db.collection('peopletable');
+      collection.find().toArray(function(err, ret) {
+        logger.debug(JSON.stringify(ret[0]));
+        res.send(ret[0]);
+        db.close();
+      });
+    });
+
   }
 };
 
--- app.js
+++ app.js
@@ -12,20 +12,18 @@ var path = require('path');
 
 var app = express();
 
-app.configure('start', function() {
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.configure('development', function() {
   app.use(express.errorHandler());
