513763ecf3564c4355f9d57b11a519fcb207f78a
Zainan Victor Zhou <zzn@zzn.im>
2013-06-12 21:19:24 -0700

    Fix url

--- shared/constants.js
+++ shared/constants.js
@@ -10,8 +10,8 @@ var HOST_ROOL_URL = process.env.HOST_ROOT_URL || 'http://localhost:5000';
 
 module.exports = {
   HOST_ROOL_URL : HOST_ROOL_URL,
-  FACEBOOK_AUTH_CALLBACK : HOST_ROOL_URL + '/facebook/callback',
-  TWITTER_AUTH_CALLBACK : HOST_ROOL_URL + '/twitter/callback',
-  GOOGLE_AUTH_RETURN : HOST_ROOL_URL + '/google/return',
-  LINKED_IN_AUTH_CALLBACK : HOST_ROOL_URL + '/google/callback'
+  FACEBOOK_AUTH_CALLBACK : HOST_ROOL_URL + '/auth/facebook/callback',
+  TWITTER_AUTH_CALLBACK : HOST_ROOL_URL + '/auth/twitter/callback',
+  GOOGLE_AUTH_RETURN : HOST_ROOL_URL + '/auth/google/return',
+  LINKED_IN_AUTH_CALLBACK : HOST_ROOL_URL + '/auth/linkedin/callback'
 };
