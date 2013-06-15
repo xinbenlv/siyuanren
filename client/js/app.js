15025deb42bbf76783d37919f1e602321e3a6f0b
Zainan Victor Zhou <zzn@zzn.im>
2013-06-15 10:57:30 -0700

    Add Carousal galary functionality

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('angular-client-side-auth', ['ngCookies'])
+angular.module('angular-client-side-auth', ['ngCookies','ui.bootstrap'])
 
     .config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
 
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -17,9 +17,7 @@ angular.module('angular-client-side-auth')
             $rootScope.error = "Failed to logout";
         });
     };
-}]);
-
-angular.module('angular-client-side-auth')
+}])
 .controller('LoginCtrl',
 ['$rootScope', '$scope', '$location', '$window', 'Auth', function($rootScope, $scope, $location, $window, Auth) {
 
@@ -41,15 +39,16 @@ angular.module('angular-client-side-auth')
     $scope.loginOauth = function(provider) {
         $window.location.href = '/auth/' + provider;
     };
-}]);
-
-angular.module('angular-client-side-auth')
+}])
 .controller('HomeCtrl',
-['$rootScope', function($rootScope) {
-
-}]);
-
-angular.module('angular-client-side-auth')
+['$rootScope','$scope', function($rootScope, $scope) {
+  $scope.myInterval = 3000;
+  $scope.slides = [
+    {image: 'http://fmn.rrimg.com/fmn056/xiaozhan/20121217/1535/xlarge_MWBM_4570000080291191.jpg', title: '年会', text: '思源年会.'},
+    {image: 'http://fmn.rrimg.com/fmn065/xiaozhan/20121217/1535/xlarge_pNP8_33e4000099ef118e.jpg', title: '朱先生', text: '思源计划发起人朱先生.!'},
+    {image: 'http://fmn.rrimg.com/fmn064/xiaozhan/20120910/2050/x_large_VnZ0_2cea00002aa71262.jpg', title: '北美思源小聚', text: '从景芳姐那儿不告而借的~~ 曲媛@6，韩赟儒@5，孔令昭@3，郝景芳@2，方铭@2~'},
+  ];
+}])
 .controller('RegisterCtrl',
 ['$rootScope', '$scope', '$location', 'Auth', function($rootScope, $scope, $location, Auth) {
     $scope.role = routingConfig.userRoles.user;
@@ -68,15 +67,10 @@ angular.module('angular-client-side-auth')
                 $rootScope.error = err;
             });
     };
-}]);
-
-angular.module('angular-client-side-auth')
+}])
 .controller('PrivateCtrl',
 ['$rootScope', function($rootScope) {
-}]);
-
-
-angular.module('angular-client-side-auth')
+}])
 .controller('AdminCtrl',
 ['$rootScope', '$scope', 'Users', function($rootScope, $scope, Users) {
     $scope.loading = true;
@@ -89,9 +83,7 @@ angular.module('angular-client-side-auth')
         $scope.loading = false;
     });
 
-}]);
-
-angular.module('angular-client-side-auth')
+}])
 .controller('PeopleTableCtrl',
 ['$rootScope', '$scope', 'Users', function($rootScope, $scope, Users) {
   peopletable.load();
