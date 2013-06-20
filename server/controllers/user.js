8c62ed673b283571f936bbb50fa148b80646789a
Zainan Victor Zhou <zzn@zzn.im>
2013-06-20 00:05:14 -0700

    Add admin page, translate to chinese

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,19 +1,21 @@
 var _ =           require('underscore')
     , User =      require('../models/User.js')
     , userRoles = require('../../client/js/routingConfig').userRoles
-
+    , constants = require('../../shared/constants')
   ;
 
 module.exports = {
     index: function(req, res) {
-        var users = User.findAll();
-        _.each(users, function(user) {
-            delete user.password;
-            for(i in constants.ENABED_PROVIDERS){
-              delete user[provider[i]];
-            }
-            delete user.google;
+        User.find({}, function(err, users){
+          _.each(users, function(user) {
+              delete user.password;
+              for(var i in constants.ENABED_PROVIDERS){
+                var provider = constants.ENABED_PROVIDERS[i];
+                delete user[provider[i]];
+              }
+              delete user.google;
+          });
+          res.json(users);
         });
-        res.json(users);
     }
 };
